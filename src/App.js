bb1fa19a8c1f8fd3b74ff7be89c6f74f44e662e0
DedSec-hun <55910572+DedSec-hun@users.noreply.github.com>
2022-11-01 15:58:57 +0100

    touch  button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [counter, setCounter] = React.useState(100); //hook jó lenne figyelni
+  const [counter, setCounter] = React.useState(0); //hook jó lenne figyelni
   const intervalRef = React.useRef(null);
-  const [dart1, setD1] = useState(10);
-  const [dart2, setD2] = useState('X');
-  const [dart3, setD3] = useState(30);
+  const [dart1, setD1] = useState(0);
+  const [dart2, setD2] = useState(0);
+  const [dart3, setD3] = useState(0);
 
   const [turn, setTurn] = useState(0);
   const [turnSum, setTurnSum] = useState(0);
@@ -28,14 +28,10 @@ setTurn(turn + 1);
 }
 
 
-  const hit = event => {
- console.log(event.target.id);
+  const hit = event => { 
+  hit_counter(event.target.id, 0);
   };
 
-  const multiplier = event => {
-    console.log(event.target.id * 2);
-     };
-  
 const dart1_zero = () => {
 setD2(1111);
 }
@@ -52,43 +48,54 @@ setBull(bull + 25)
 if (bull == 50) setBull(0);
   };
 
-  const startCounter = () => {
-
-   
+  const startCounter = (event) => {
 
     if (intervalRef.current) return;
     intervalRef.current = setInterval(() => {
       setCounter((prevCounter) => prevCounter + 1);
     }, 10);
-  };
+  };  
 
-  const stopCounter = () => {
+  const stopCounter = (event) => {
     if (intervalRef.current) {
       clearInterval(intervalRef.current);
       intervalRef.current = null;
-      
+      hit_counter(event.target.id, counter)
       setCounter(0);
     }
   };
 
+function hit_counter(dart, multiplier_counter)
+{
+let multiplier = 0;
+if (multiplier_counter > 20 && multiplier_counter < 100) {
+  multiplier = 2;
+}
+else if (multiplier_counter >= 100){
+  multiplier = 3;
+}
+else if (multiplier_counter == 0) {
+multiplier = 1;
+}
 
-  useEffect(() => {
-    if(counter < 0 && counter < 20) {
-      setD2('Simple');
-          }
-          else if (counter > 20 && counter < 100) {
-            setD2('Double');
-          }
-          else if (counter > 100) {
-            setD2('Triple');
-          }
-          else if (counter == 0) {
-            
-          }
-  }, [counter]);
+
+
+  if (dart1 == 0) {
+    setD1(dart * multiplier);
+    return;
+  }
+  if (dart2 == 0) {
+    setD2(dart * multiplier);
+    return;
+  }
+  if (dart3 == 0) {
+    setD3(dart * multiplier);
+    return;
+  }
+}
 
   const elementStyle = {
-    width: 1 + `${counter}px`,
+    width: `${counter}%`, 
   };
   
   return (
@@ -97,9 +104,10 @@ if (bull == 50) setBull(0);
 
          <h1> Kör:  { Math.floor(turn - turn/2)}</h1>
         <h1> Kör szumma:  {turnSum}</h1>
-        
-        <button
-        style={elementStyle}>BtnPress- {dart2}</button>
+        <h1>{dart1}</h1>
+        <h1>{dart2}</h1>
+        <h1>{dart3}</h1>
+        <button className='multiplier-counter' style={elementStyle}>{counter}</button>
     
 <ul className='board'>
 {
@@ -107,9 +115,9 @@ slices.map(function(item, i){
   let design_slice = i % 2 ? "slice-contents" : "slice-contents-wheat";
   
  return <li className='slice'>
-    <div className={design_slice} id={item} onMouseDown={startCounter}
-        onMouseUp={stopCounter}
-        onMouseLeave={stopCounter}> <p>{item}</p> </div>
+    <div className={design_slice} id={item} onClick={hit} onTouchStart={startCounter}
+        onTouchEnd={stopCounter}
+       > <p>{item}</p> </div>
     </li>
 })
 
@@ -117,9 +125,7 @@ slices.map(function(item, i){
 <span className={'bull'} onClick={bully}> <h3>{ bull == 0 ?  '' : bull}</h3>  </span>
 </ul>
 
-<button onMouseDown={startCounter}
-        onMouseUp={stopCounter}
-        onMouseLeave={stopCounter}>Score</button>
+<button>Score</button>
 <button onClick={endTurn}> Next</button>
 
 <div> <h1 className={turn % 2 == 0 ? 'active' : 'not-active'} >Jatekos1: {p1}</h1><h1 className={turn % 2 != 0 ? 'active' : 'not-active'}>Jatekos2: {p2}</h1></div>
